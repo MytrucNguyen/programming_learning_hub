30a443496a709cbcecbb0bb0d5601fa684f9191e
Mytruc Nguyen <80004058+MytrucNguyen@users.noreply.github.com>
2024-09-18 15:07:01 -0700

    Section 3: React Basic

--- learn_to_build_an_e-commerce_store/ReStore/client/src/App.tsx
+++ learn_to_build_an_e-commerce_store/ReStore/client/src/App.tsx
@@ -1,8 +1,20 @@
+const products = [
+  { name: 'product1', price: 100.00 },
+  { name: 'product2', price: 200.00 },
+  { name: 'product3', price: 300.00 },
+]
 function App() {
 
   return (
-    <div>
+    <div className="app">
       <h1>Re-Store</h1>
+      <ul>
+        {products.map((item =>
+          <li key={item.name}>
+            {item.name} - ${item.price}
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
