6fccdd84a93ee1d8016df463d86e9cb2adcff4c4
Mytruc Nguyen <80004058+MytrucNguyen@users.noreply.github.com>
2024-09-17 18:59:40 -0700

    Section 2: API Basics

--- learn_to_build_an_e-commerce_store/ReStore/API/Data/StoreContext.cs
+++ learn_to_build_an_e-commerce_store/ReStore/API/Data/StoreContext.cs
@@ -3,11 +3,8 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Data
 {
-    public class StoreContext : DbContext
+    public class StoreContext(DbContextOptions options) : DbContext(options)
     {
-        public StoreContext(DbContextOptions options) : base(options)
-        {
-        }
         public DbSet<Product> Products { get; set; }
     }
 }
