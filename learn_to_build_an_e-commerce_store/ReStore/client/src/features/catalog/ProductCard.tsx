d7ab08b80fdbf2af4c7e51b932dd9456b6bbc62c
Mytruc Nguyen <80004058+MytrucNguyen@users.noreply.github.com>
2024-09-24 16:15:29 -0700

    Section 3: React Basic - Adding cards for our individual products

--- learn_to_build_an_e-commerce_store/ReStore/client/src/features/catalog/ProductCard.tsx
+++ learn_to_build_an_e-commerce_store/ReStore/client/src/features/catalog/ProductCard.tsx
@@ -1,4 +1,4 @@
-import { ListItem, ListItemAvatar, Avatar, ListItemText } from "@mui/material";
+import { Button, Card, CardActions, CardContent, CardMedia, Typography } from "@mui/material";
 import { Product } from "../../app/models/product";
 
 interface ProductCardProps {
@@ -7,11 +7,32 @@ interface ProductCardProps {
 
 export default function ProductCard({ product }: ProductCardProps) {
     return (
-        <ListItem key={product.id}>
-            <ListItemAvatar>
-                <Avatar src={product.pictureUrl} />
-            </ListItemAvatar>
-            <ListItemText> {product.name} - ${product.price}</ListItemText>
-        </ListItem>
+        <Card key={product.id}>
+            <CardMedia
+                sx={{ height: 140 }}
+                image={product.pictureUrl}
+                title={product.name}
+            />
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                    {product.name}
+                </Typography>
+                <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                    {product.description}
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button size="small">Share</Button>
+                <Button size="small">Learn More</Button>
+            </CardActions>
+        </Card>
+
+
+        // <ListItem key={product.id}>
+        //     <ListItemAvatar>
+        //         <Avatar src={product.pictureUrl} />
+        //     </ListItemAvatar>
+        //     <ListItemText> {product.name} - ${product.price}</ListItemText>
+        // </ListItem>
     )
 }
