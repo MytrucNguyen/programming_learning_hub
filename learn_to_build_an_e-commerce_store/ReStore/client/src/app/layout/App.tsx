578491da907fb6b8209e059a57db18b5904add3e
Mytruc Nguyen <80004058+MytrucNguyen@users.noreply.github.com>
2024-09-24 16:03:11 -0700

    Section 3: React Basic - Add AppBar

--- learn_to_build_an_e-commerce_store/ReStore/client/src/app/layout/App.tsx
+++ learn_to_build_an_e-commerce_store/ReStore/client/src/app/layout/App.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react"
 import { Product } from "../models/product";
 import Catalog from "../../features/catalog/Catalog";
-import { Typography } from "@mui/material";
+import { Container, CssBaseline } from "@mui/material";
+import Header from "./Header";
 
 function App() {
   const [products, setProducts] = useState<Product[]>([]);
@@ -25,14 +26,16 @@ function App() {
   }
 
   return (
-    <div className="app">
-      <Typography variant="h1">Re-Store</Typography>
-      <Catalog
-        products={products}
-        addProduct={addProduct}
-      />
-
-    </div>
+    <>
+      <CssBaseline />
+      <Header />
+      <Container>
+        <Catalog
+          products={products}
+          addProduct={addProduct}
+        />
+      </Container>
+    </>
   )
 }
 
