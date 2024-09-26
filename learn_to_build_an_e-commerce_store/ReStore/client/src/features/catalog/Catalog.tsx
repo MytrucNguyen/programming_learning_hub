aeb726fdc0d5a301ba30ad70dad9ec9e320b3e2f
Mytruc Nguyen <80004058+MytrucNguyen@users.noreply.github.com>
2024-09-25 17:17:08 -0700

    Section 5: Error Handling - Centralising the axios requests

--- learn_to_build_an_e-commerce_store/ReStore/client/src/features/catalog/Catalog.tsx
+++ learn_to_build_an_e-commerce_store/ReStore/client/src/features/catalog/Catalog.tsx
@@ -1,14 +1,13 @@
 import { useEffect, useState } from "react"
 import ProductList from "./ProductList";
 import { Product } from "../../app/models/product";
+import agent from "../../app/api/agent";
 
 export default function Catalog() {
     const [products, setProducts] = useState<Product[]>([]);
 
     useEffect(() => {
-        fetch('http://localhost:5000/api/products')
-            .then(response => response.json())
-            .then(data => setProducts(data));
+        agent.Catalog.list().then(products => setProducts(products))
     }, [])
 
     return (
--- learn_to_build_an_e-commerce_store/ReStore/client/src/features/catalog/ProductDetails.tsx
+++ learn_to_build_an_e-commerce_store/ReStore/client/src/features/catalog/ProductDetails.tsx
@@ -1,8 +1,8 @@
 import { Divider, Grid, Table, TableBody, TableCell, TableContainer, TableRow, Typography } from "@mui/material";
-import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Product } from "../../app/models/product";
+import agent from "../../app/api/agent";
 
 export default function ProductDetails() {
     const { id } = useParams<{ id: string }>();
@@ -10,10 +10,10 @@ export default function ProductDetails() {
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        axios.get(`http://localhost:5000/api/products/${id}`)
-            .then(response => setProduct(response.data))
+        agent.Catalog.details(Number(id))
+            .then(response => setProduct(response))
             .catch(error => console.log(error))
-            .finally(() => setLoading(false));
+            .finally(() => setLoading(false))
     }, [id])
 
     if (loading) return <h3>Loading...</h3>
