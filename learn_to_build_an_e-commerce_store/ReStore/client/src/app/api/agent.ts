b1e7714a29d0e965cbf0c87942fbac83e2ab502e
Mytruc Nguyen <80004058+MytrucNguyen@users.noreply.github.com>
2024-09-26 09:52:06 -0700

    Section 5: Error Handling - Handling validation errors

--- learn_to_build_an_e-commerce_store/ReStore/client/src/app/api/agent.ts
+++ learn_to_build_an_e-commerce_store/ReStore/client/src/app/api/agent.ts
@@ -11,6 +11,17 @@ axios.interceptors.response.use(response => {
     const { data, status } = error.response as AxiosResponse;
     switch (status) {
         case 400:
+            if (data.errors) {
+                const modelStateErrors: string[] = [];
+                for (const key in data.errors) {
+                    if (data.errors[key]) {
+                        modelStateErrors.push(data.errors[key]);
+                    }
+                }
+
+                throw modelStateErrors.flat();
+            }
+
             toast.error(data.title)
             break;
         case 401:
