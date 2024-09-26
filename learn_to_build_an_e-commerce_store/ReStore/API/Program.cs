ae133ca28c8526238247a72b9a7f430ed215e86b
Mytruc Nguyen <80004058+MytrucNguyen@users.noreply.github.com>
2024-09-25 17:00:51 -0700

    Section 5: Error Handling - Adding Exception handling middleware

--- learn_to_build_an_e-commerce_store/ReStore/API/Program.cs
+++ learn_to_build_an_e-commerce_store/ReStore/API/Program.cs
@@ -1,4 +1,5 @@
 using API.Data;
+using API.MiddleWare;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -17,6 +18,8 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
