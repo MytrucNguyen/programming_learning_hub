32cc80513adf2f784c6b63667e37c38fb30e9083
Mytruc Nguyen <80004058+MytrucNguyen@users.noreply.github.com>
2024-09-27 19:57:53 -0700

    Section 6: Adding the shopping cart feature - EF Relationships

--- learn_to_build_an_e-commerce_store/ReStore/API/Entities/BasketItem.cs
+++ learn_to_build_an_e-commerce_store/ReStore/API/Entities/BasketItem.cs
@@ -1,5 +1,8 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace API.Entities
 {
+    [Table("BasketItems")]
     public class BasketItem
     {
         public int Id { get; set; }
@@ -8,5 +11,7 @@ namespace API.Entities
         // navigation properties
         public int ProductId { get; set; }
         public Product Product { get; set; }
+        public int BasketId { get; set; }
+        public Basket Basket { get; set; }
     }
 }
