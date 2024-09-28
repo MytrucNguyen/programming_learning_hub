e1eb4d52da601718f2efd519c8d9e15addf448eb
Mytruc Nguyen <80004058+MytrucNguyen@users.noreply.github.com>
2024-09-28 16:42:21 -0700

    Section 6: Adding the shopping cart feature - Shaping the data to return

--- learn_to_build_an_e-commerce_store/ReStore/API/Controllers/BasketController.cs
+++ learn_to_build_an_e-commerce_store/ReStore/API/Controllers/BasketController.cs
@@ -1,4 +1,5 @@
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Mvc;
@@ -16,13 +17,27 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<Basket>> GetBasket()
+        public async Task<ActionResult<BasketDto>> GetBasket()
         {
             var basket = await RetrieveBasket();
 
             if (basket == null) return NotFound();
 
-            return basket;
+            return new BasketDto
+            {
+                Id = basket.Id,
+                BuyerId = basket.BuyerId,
+                Items = basket.Items.Select(item => new BasketItemDto
+                {
+                    ProductId = item.ProductId,
+                    Name = item.Product.Name,
+                    Price = item.Product.Price,
+                    PictureUrl = item.Product.PictureUrl,
+                    Type = item.Product.Type,
+                    Brand = item.Product.Brand,
+                    Quantity = item.Quantity
+                }).ToList()
+            };
         }
 
         [HttpPost]
