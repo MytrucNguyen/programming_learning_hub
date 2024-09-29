ea0cf419ef5be1e23f7ddc0288be01d5f7c4d85f
Mytruc Nguyen <80004058+MytrucNguyen@users.noreply.github.com>
2024-09-29 16:10:35 -0700

    Section 6: Adding the shopping cart feature - Using CreatedAtRoute

--- learn_to_build_an_e-commerce_store/ReStore/API/Controllers/BasketController.cs
+++ learn_to_build_an_e-commerce_store/ReStore/API/Controllers/BasketController.cs
@@ -17,32 +17,18 @@ namespace API.Controllers
 
         }
 
-        [HttpGet]
+        [HttpGet(Name = "GetBasket")]
         public async Task<ActionResult<BasketDto>> GetBasket()
         {
             var basket = await RetrieveBasket();
 
             if (basket == null) return NotFound();
 
-            return new BasketDto
-            {
-                Id = basket.Id,
-                BuyerId = basket.BuyerId,
-                Items = basket.Items.Select(item => new BasketItemDto
-                {
-                    ProductId = item.ProductId,
-                    Name = item.Product.Name,
-                    Price = item.Product.Price,
-                    PictureUrl = item.Product.PictureUrl,
-                    Type = item.Product.Type,
-                    Brand = item.Product.Brand,
-                    Quantity = item.Quantity
-                }).ToList()
-            };
+            return MapBasketToDto(basket);
         }
 
         [HttpPost]
-        public async Task<ActionResult> AddItemToBasket(int productId, int quantity)
+        public async Task<ActionResult<BasketDto>> AddItemToBasket(int productId, int quantity)
         {
             // get bakset || create basket
             var basket = await RetrieveBasket();
@@ -58,6 +44,8 @@ namespace API.Controllers
             // save changes
             var result = await _context.SaveChangesAsync() > 0;
             if (result) return StatusCode(201);
+
+            if (result) return CreatedAtRoute("GetBasket", MapBasketToDto(basket));
 
             return BadRequest(new ProblemDetails { Title = "Problem saving item to basket" });
         }
@@ -96,6 +84,25 @@ namespace API.Controllers
             var basket = new Basket { BuyerId = buyerId };
             _context.Baskets.Add(basket);
             return basket;
+        }
+
+        private static BasketDto MapBasketToDto(Basket basket)
+        {
+            return new BasketDto
+            {
+                Id = basket.Id,
+                BuyerId = basket.BuyerId,
+                Items = basket.Items.Select(item => new BasketItemDto
+                {
+                    ProductId = item.ProductId,
+                    Name = item.Product.Name,
+                    Price = item.Product.Price,
+                    PictureUrl = item.Product.PictureUrl,
+                    Type = item.Product.Type,
+                    Brand = item.Product.Brand,
+                    Quantity = item.Quantity
+                }).ToList()
+            };
         }
 
     }
