d2fd9cce08c8a7323aeebe2280a1b47efa6b61fc
Mytruc Nguyen <80004058+MytrucNguyen@users.noreply.github.com>
2024-10-05 14:05:42 -0700

    Section 6: Adding the shopping cart feature - Adding the remove item functionality

--- learn_to_build_an_e-commerce_store/ReStore/client/src/features/basket/BasketPage.tsx
+++ learn_to_build_an_e-commerce_store/ReStore/client/src/features/basket/BasketPage.tsx
@@ -1,9 +1,29 @@
-import { IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
-import { Delete } from "@mui/icons-material";
+import { Box, IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
+import { Add, Delete, Remove } from "@mui/icons-material";
 import { useStoreContext } from "../../app/context/StoreContext";
+import { useState } from "react";
+import agent from "../../app/api/agent";
+import { LoadingButton } from "@mui/lab";
 
 export default function BasketPage() {
-    const { basket } = useStoreContext();
+    const { basket, setBasket, removeItem } = useStoreContext();
+    const [loading, setLoading] = useState(false);
+
+    function handleAddItem(productId: number) {
+        setLoading(true);
+        agent.Basket.addItem(productId)
+            .then(basket => setBasket(basket))
+            .catch(error => console.log(error))
+            .finally(() => setLoading(false))
+    }
+
+    function handleRemoveItem(productId: number, quantity = 1) {
+        setLoading(true);
+        agent.Basket.removeItem(productId, quantity)
+            .then(() => removeItem(productId, quantity))
+            .catch(error => console.log(error))
+            .finally(() => setLoading(false))
+    }
 
     if (!basket) return <Typography variant="h3">Your basket is empty</Typography>
 
@@ -14,7 +34,7 @@ export default function BasketPage() {
                     <TableRow>
                         <TableCell>Product</TableCell>
                         <TableCell align="right">Price</TableCell>
-                        <TableCell align="right">Quantity</TableCell>
+                        <TableCell align="center">Quantity</TableCell>
                         <TableCell align="right">Subtotal</TableCell>
                         <TableCell align="right"></TableCell>
                     </TableRow>
@@ -26,15 +46,26 @@ export default function BasketPage() {
                             sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                         >
                             <TableCell component="th" scope="row">
-                                {item.name}
+                                <Box display={'flex'} alignItems={'center'}>
+                                    <img src={item.pictureUrl} alt={item.name} style={{ height: 50, marginRight: 20 }} />
+                                    {item.name}
+                                </Box>
                             </TableCell>
                             <TableCell align="right">${(item.price / 100).toFixed(2)}</TableCell>
-                            <TableCell align="right">{item.quantity}</TableCell>
+                            <TableCell align="center">
+                                <LoadingButton loading={loading} color="error" onClick={() => handleRemoveItem(item.productId)}>
+                                    <Remove />
+                                </LoadingButton>
+                                {item.quantity}
+                                <LoadingButton loading={loading} color="secondary" onClick={() => handleAddItem(item.productId)}>
+                                    <Add />
+                                </LoadingButton>
+                            </TableCell>
                             <TableCell align="right">{((item.price / 100) * item.quantity).toFixed(2)}</TableCell>
                             <TableCell align="right">
-                                <IconButton color="error">
+                                <LoadingButton loading={loading} color="error" onClick={() => handleRemoveItem(item.productId, item.quantity)}>
                                     <Delete />
-                                </IconButton>
+                                </LoadingButton>
                             </TableCell>
                         </TableRow>
                     ))}
