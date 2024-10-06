23707da4741b2b260f884ebe854af91e9fe82188
Mytruc Nguyen <80004058+MytrucNguyen@users.noreply.github.com>
2024-10-06 14:52:12 -0700

    Section 6: Adding the shopping cart feature - Adding a checkout page

---
+++ learn_to_build_an_e-commerce_store/ReStore/client/src/features/checkout/CheckoutPage.tsx
@@ -0,0 +1,9 @@
+import { Typography } from "@mui/material";
+
+export default function CheckoutPage() {
+    return (
+        <Typography variant="h3">
+            Checkout Page
+        </Typography>
+    )
+}
