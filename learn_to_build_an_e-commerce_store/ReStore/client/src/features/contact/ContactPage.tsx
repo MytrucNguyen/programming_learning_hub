2e961e228d3b972e72d51e82e48cc8cf00722188
Mytruc Nguyen <80004058+MytrucNguyen@users.noreply.github.com>
2024-10-08 12:43:44 -0700

    Section 7: Redux - Redux Actions

--- learn_to_build_an_e-commerce_store/ReStore/client/src/features/contact/ContactPage.tsx
+++ learn_to_build_an_e-commerce_store/ReStore/client/src/features/contact/ContactPage.tsx
@@ -1,8 +1,9 @@
-import { Typography } from "@mui/material";
-import { useSelector } from "react-redux";
-import { CounterState } from "./counterReducer";
+import { Button, ButtonGroup, Typography } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import { CounterState, DECREMENT, INCREMENT } from "./counterReducer";
 
 export default function ContactPage() {
+    const dispatch = useDispatch();
     const { data, title } = useSelector((state: CounterState) => state);
 
     return (
@@ -13,6 +14,10 @@ export default function ContactPage() {
             <Typography variant="h5">
                 {data}
             </Typography>
+            <ButtonGroup>
+                <Button onClick={() => dispatch({ type: DECREMENT })} variant="contained" color="error">Decrement</Button>
+                <Button onClick={() => dispatch({ type: INCREMENT })} variant="contained" color="primary">Increment</Button>
+            </ButtonGroup>
         </>
     )
 }
--- learn_to_build_an_e-commerce_store/ReStore/client/src/features/contact/counterReducer.ts
+++ learn_to_build_an_e-commerce_store/ReStore/client/src/features/contact/counterReducer.ts
@@ -1,3 +1,16 @@
+export const INCREMENT = 'INCREMENT';
+export const DECREMENT = 'DECREMENT';
+
+interface IncrementAction {
+    type: typeof INCREMENT;
+}
+
+interface DecrementAction {
+    type: typeof DECREMENT;
+}
+
+type CounterAction = IncrementAction | DecrementAction;
+
 export interface CounterState {
     data: number;
     title: string;
@@ -8,6 +21,22 @@ const initialState: CounterState = {
     title: 'YARC',
 }
 
-export default function counterReducer(state = initialState, action: any) {
-    return state;
+export default function counterReducer(
+    state = initialState,
+    action: CounterAction
+): CounterState {
+    switch (action.type) {
+        case INCREMENT:
+            return {
+                ...state,
+                data: state.data + 1,
+            };
+        case DECREMENT:
+            return {
+                ...state,
+                data: state.data - 1,
+            };
+        default:
+            return state;
+    }
 }
