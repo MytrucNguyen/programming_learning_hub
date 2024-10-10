41d3c7dcb03c4e4fc440adac502f0a57c58e9f09
Mytruc Nguyen <80004058+MytrucNguyen@users.noreply.github.com>
2024-10-09 20:13:28 -0700

    Fix error

--- learn_to_build_an_e-commerce_store/ReStore/client/src/app/store/configureStore.ts
+++ learn_to_build_an_e-commerce_store/ReStore/client/src/app/store/configureStore.ts
@@ -1,4 +1,4 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../../features/contact/counterReducer";
 
 export const store = configureStore({
