1cbbaa491852432ae11d328254f41164c8c3c663
Mytruc Nguyen <80004058+MytrucNguyen@users.noreply.github.com>
2024-11-03 18:02:01 -0700

    Section 7: Using entity adapter selectors

--- learn_to_build_an_e-commerce_store/ReStore/client/src/features/catalog/catalogSlice.ts
+++ learn_to_build_an_e-commerce_store/ReStore/client/src/features/catalog/catalogSlice.ts
@@ -1,6 +1,7 @@
 import { createAsyncThunk, createEntityAdapter, createSlice } from "@reduxjs/toolkit";
 import { Product } from "../../app/models/product";
 import agent from "../../app/api/agent";
+import { RootState } from "../../app/store/configureStore";
 
 const productsAdapter = createEntityAdapter<Product>();
 
@@ -18,7 +19,7 @@ export const fetchProductsAsync = createAsyncThunk<Product[]>(
 export const catalogSlice = createSlice({
     name: 'catalog',
     initialState: productsAdapter.getInitialState({
-        productsLoded: false,
+        productsLoaded: false,
         status: 'idle',
     }),
     reducers: {},
@@ -29,10 +30,12 @@ export const catalogSlice = createSlice({
         builder.addCase(fetchProductsAsync.fulfilled, (state, action) => {
             productsAdapter.setAll(state, action.payload);
             state.status = 'idle';
-            state.productsLoded = true;
+            state.productsLoaded = true;
         })
         builder.addCase(fetchProductsAsync.rejected, (state) => {
             state.status = 'idle';
         })
     })
 });
+
+export const productSelectors = productsAdapter.getSelectors((state: RootState) => state.catalog);
