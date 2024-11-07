5f9ef9f773568e352e91f6bba53449c1469bcdbc
Mytruc Nguyen <80004058+MytrucNguyen@users.noreply.github.com>
2024-11-07 09:16:25 -0700

    Section 8: Adding sorting to the API

--- learn_to_build_an_e-commerce_store/ReStore/API/Controllers/ProductsController.cs
+++ learn_to_build_an_e-commerce_store/ReStore/API/Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
 using API.Data;
 using API.Entities;
+using API.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,9 +11,13 @@ namespace API.Controllers
         private readonly StoreContext _context = context;
 
         [HttpGet]
-        public async Task<ActionResult<List<Product>>> GetProducts()
+        public async Task<ActionResult<List<Product>>> GetProducts(string orderBy)
         {
-            return await _context.Products.ToListAsync();
+            var query = _context.Products
+            .Sort(orderBy)
+            .AsQueryable();
+
+            return await query.ToListAsync();
         }
 
         [HttpGet("{id}")]
