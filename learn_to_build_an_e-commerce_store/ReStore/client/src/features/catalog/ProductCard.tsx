d3239e30f84d1419b6017cf2a0c717d1ae11fd02
Mytruc Nguyen <80004058+MytrucNguyen@users.noreply.github.com>
2024-11-11 16:57:38 -0700

    Fix loader on Product card

--- learn_to_build_an_e-commerce_store/ReStore/client/src/features/catalog/ProductCard.tsx
+++ learn_to_build_an_e-commerce_store/ReStore/client/src/features/catalog/ProductCard.tsx
@@ -44,7 +44,7 @@ export default function ProductCard({ product }: ProductCardProps) {
                 </Typography>
             </CardContent>
             <CardActions>
-                <LoadingButton loading={status.includes('pendingAddItem' + product.id)} onClick={() => dispatch(addBasketItemAsync({ productId: product.id }))} size="small" >Add to cart</LoadingButton>
+                <LoadingButton loading={status === ('pendingAddItem' + product.id)} onClick={() => dispatch(addBasketItemAsync({ productId: product.id }))} size="small" >Add to cart</LoadingButton>
                 <Button size="small" component={Link} to={`/catalog/${product.id}`}>View</Button>
             </CardActions>
         </Card>
