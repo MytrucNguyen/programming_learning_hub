fa9dad0e8885ed7264a5d68b9b328cfcaffa5ff9
Mytruc Nguyen <80004058+MytrucNguyen@users.noreply.github.com>
2024-11-10 18:56:37 -0700

    Section 8: Creating a class that extends our List into a PagedList

--- learn_to_build_an_e-commerce_store/ReStore/API/Controllers/ProductsController.cs
+++ learn_to_build_an_e-commerce_store/ReStore/API/Controllers/ProductsController.cs
@@ -1,6 +1,8 @@
+using System.Text.Json;
 using API.Data;
 using API.Entities;
 using API.Extensions;
+using API.RequestHelpers;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,20 +13,19 @@ namespace API.Controllers
         private readonly StoreContext _context = context;
 
         [HttpGet]
-        public async Task<ActionResult<List<Product>>> GetProducts(
-            string orderBy,
-            string searchTerm,
-            string brands,
-            string types
-            )
+        public async Task<ActionResult<PagedList<Product>>> GetProducts([FromQuery] ProductParams productParams)
         {
             var query = _context.Products
-            .Sort(orderBy)
-            .Search(searchTerm)
-            .Filter(brands, types)
+            .Sort(productParams.OrderBy)
+            .Search(productParams.SearchTerm)
+            .Filter(productParams.Brands, productParams.Types)
             .AsQueryable();
 
-            return await query.ToListAsync();
+            var products = await PagedList<Product>.ToPagedList(query, productParams.PageNumber, productParams.PageSize);
+
+            Response.AddPaginationHeader(products.MetaData);
+
+            return products;
         }
 
         [HttpGet("{id}")]
