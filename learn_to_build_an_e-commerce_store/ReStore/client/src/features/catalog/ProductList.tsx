0bd889113ce3f34f09bbb22f269fc436fd12ec74
Mytruc Nguyen <80004058+MytrucNguyen@users.noreply.github.com>
2024-11-18 12:12:15 -0700

    Section 8: Cleaning up the loading indicators in the catalog

--- learn_to_build_an_e-commerce_store/ReStore/client/src/features/catalog/ProductList.tsx
+++ learn_to_build_an_e-commerce_store/ReStore/client/src/features/catalog/ProductList.tsx
@@ -1,17 +1,24 @@
 import { Grid } from "@mui/material";
 import { Product } from "../../app/models/product";
 import ProductCard from "./ProductCard";
+import { useAppSelector } from "../../app/store/configureStore";
+import ProductCardSkeleton from "./ProductCardSkeleton";
 
 interface ProductListProps {
     products: Product[];
 }
 
 export default function ProductList({ products }: ProductListProps) {
+    const { productsLoaded } = useAppSelector(state => state.catalog);
     return (
         <Grid container spacing={4}>
             {products.map((product =>
                 <Grid item sm={4} key={product.id}>
-                    <ProductCard product={product} />
+                    {!productsLoaded ? (
+                        <ProductCardSkeleton />
+                    ) : (
+                        <ProductCard product={product} />
+                    )}
                 </Grid>
             ))}
         </Grid>
