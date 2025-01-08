0168ce3a5e45e6e12a8fddcd1d5bb6ff6732ffe9
Mytruc Nguyen <80004058+MytrucNguyen@users.noreply.github.com>
2025-01-08 10:43:00 -0700

    Section 10 (Section 23 Legacy): Shaping the order data

---
+++ learn_to_build_an_e-commerce_store/ReStore/API/DTOs/OrderDto.cs
@@ -0,0 +1,18 @@
+using API.Entities.OrderAggregate;
+
+namespace API.DTOs
+{
+    public class OrderDto
+    {
+        public int Id { get; set; }
+        public string BuyerId { get; set; }
+
+        public ShippingAddress ShippingAddress { get; set; }
+        public DateTime OrderDate { get; set; } = DateTime.Now;
+        public List<OrderItemDto> OrderItems { get; set; }
+        public long Subtotal { get; set; }
+        public long DeliveryFee { get; set; }
+        public string OrderStatus { get; set; }
+        public long Total { get; set; }
+    }
+}
